27841e45c7b6f8f4ce89aac206d63cfb4c7272b1
ANDERSON NAZARENO ALCANTARA DE OLIVEIRA <andervilo@hotmail.com>
2026-09-13 15:41:09 -0300

    Adiciona suporte offline

---
+++ autonomia-motinha/sw.js
@@ -0,0 +1,5 @@
+const CACHE="autonomia-motinha-v2";
+const ASSETS=["./","./index.html","./manifest.webmanifest","./icon.svg"];
+self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim()});
+self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{const cp=r.clone();caches.open(CACHE).then(k=>k.put(e.request,cp));return r}).catch(()=>caches.match("./index.html"))))});
